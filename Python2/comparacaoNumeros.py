cf2248020d826b15100d618eed1849c040aca84b
Rogersamacedo <41793312+Rogersamacedo@users.noreply.github.com>
2021-09-13 20:35:35 -0300

    meu primeiro commit

---
+++ Python2/comparacaoNumeros.py
@@ -0,0 +1,10 @@
+#desafio 38
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite mais um nuemro: "))
+if num1 > num2:
+    print("O primeiro nuemro é maior que  segundo: {} ".format(num1))
+elif num1 < num2:
+    print("O segundo numero é maior que o primeiro: {}".format(num2))
+else :
+    print(" Não existe valor maior, os dois são iguais: {}".format(num1 == num2))
+                   
